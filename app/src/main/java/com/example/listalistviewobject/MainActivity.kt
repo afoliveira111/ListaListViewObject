ea72e094270aa08dcd4e105ea0dcf76c3710a0ed
Felipe <felipetermometria@hotmail.com>
2024-01-19 16:20:54 -0300

    Edit bindind create lista

--- app/src/main/java/com/example/listalistviewobject/MainActivity.kt
+++ app/src/main/java/com/example/listalistviewobject/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.listalistviewobject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listalistviewobject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val listaUtilizadores = ArrayList<Utilizador>()
+        listaUtilizadores.add(Utilizador("user", "pass"))
+        listaUtilizadores.add(Utilizador("user", "pass"))
+        listaUtilizadores.add(Utilizador("user", "pass"))
     }
 }
