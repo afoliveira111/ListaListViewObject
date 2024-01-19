090c5d030f36ad187c40750e1ac0b6a1cafa8b13
Felipe <felipetermometria@hotmail.com>
2024-01-19 16:50:48 -0300

    edit main

--- app/src/main/java/com/example/listalistviewobject/MainActivity.kt
+++ app/src/main/java/com/example/listalistviewobject/MainActivity.kt
@@ -8,6 +8,7 @@ import com.example.listalistviewobject.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var pos = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +24,28 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaUtilizadores)
         binding.listViewUtilizadores.adapter = adapter
+
+        binding.listViewUtilizadores.setOnItemClickListener { _, _, position, _ -> }
+            binding.editUsername.setText(listaUtilizadores.get(position).username)
+            binding.editUsername.setText(listaUtilizadores.get(position).password)
+            pos = position
+
+        binding.buttonInserir.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editPassword.text.toString().trim()
+
+            if (!username.isEmpty() && !password.isEmpty()) {
+                listaUtilizadores.add(Utilizador(username, password))
+                adapter.notifyDataSetChanged()
+                binding.editUsername.setText("")
+                binding.editPassword.setText("")
+            }
+
+        }
+        binding.buttonEditar.setOnClickListener {  }
+        binding.buttonExcluir.setOnClickListener {  }
     }
+
 }
+
+
--- app/src/main/java/com/example/listalistviewobject/Utilizador.kt
+++ app/src/main/java/com/example/listalistviewobject/Utilizador.kt
@@ -2,4 +2,7 @@ package com.example.listalistviewobject
 
 class Utilizador(val username : String, val password : String) {
 
+    override fun toString(): String {
+        return username
+    }
 }
