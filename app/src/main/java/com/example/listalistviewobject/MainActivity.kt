737fd74782f4fce5cc73fe80d1b554f84676dc7e
Felipe <felipetermometria@hotmail.com>
2024-01-23 18:17:30 -0300

    Create button Delete All

--- app/src/main/java/com/example/listalistviewobject/MainActivity.kt
+++ app/src/main/java/com/example/listalistviewobject/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.listalistviewobject
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.listalistviewobject.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 
 class MainActivity : AppCompatActivity() {
 
