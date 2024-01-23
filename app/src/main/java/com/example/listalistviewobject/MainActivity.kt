56571cd09f845f959140149f4e431fc8b9dee12a
Felipe <felipetermometria@hotmail.com>
2024-01-23 17:55:38 -0300

    binding and position

--- app/src/main/java/com/example/listalistviewobject/MainActivity.kt
+++ app/src/main/java/com/example/listalistviewobject/MainActivity.kt
@@ -15,7 +15,7 @@ import kotlinx.coroutines.withContext
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var pos = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +31,32 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listaUtilizadores)
         binding.listViewUtilizadores.adapter = adapter
 
-        binding.listViewUtilizadores.setOnItemClickListener { parent, view, position, id ->
+        binding.listViewUtilizadores.setOnItemClickListener { _, _, position, _ ->
             binding.editUsername.setText(listaUtilizadores.get(position).username)
             binding.editUsername.setText(listaUtilizadores.get(position).password)
+            pos = position
+        }
 
+        binding.buttonAdd.setOnClickListener {
+            val username = binding.editUsername.text.toString().trim()
+            val password = binding.editUsername.text.toString().trim()
+
+            if(!username.isEmpty() && !password.isEmpty()) {
+                listaUtilizadores.add(Utilizador(username, password))
+                adapter.notifyDataSetChanged()
+                binding.editUsername.setText("")
+                binding.editPassword.setText("")
+            }
+        }
+        binding.buttonUpdate.setOnClickListener {  }
+        binding.buttonDelete.setOnClickListener {
+            if(pos >= 0) {
+                listaUtilizadores.removeAt(pos)
+                adapter.notifyDataSetChanged()
+                binding.editUsername.setText("")
+                binding.editPassword.setText("")
+                pos = -1
+            }
         }
     }
 }
